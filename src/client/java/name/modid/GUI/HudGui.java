0a79c8fced3462da5db7d4cf690773104943ec40
Jaiden Khosla <74748528+JaidenKhosla@users.noreply.github.com>
2025-03-16 10:46:41 -0500

    Working on getting text to work.

--- src/client/java/name/modid/GUI/HudGui.java
+++ src/client/java/name/modid/GUI/HudGui.java
@@ -6,14 +6,18 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.HudLayerRegistrationCallback;
 import net.fabricmc.fabric.api.client.rendering.v1.IdentifiedLayer;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.LayeredDrawer;
 import net.minecraft.client.render.RenderTickCounter;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Util;
 import net.minecraft.util.math.ColorHelper;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Position;
+import org.joml.Matrix4f;
 import org.joml.Vector2d;
 
 
@@ -27,7 +31,7 @@ public class HudGui {
     //scaled from the top left corner of element
     private static Vector2d DIMENSIONS = new Vector2d(100,25);
     //Positions shift from the top left corner of element
-    private static Vector2d TARGET_POS = new Vector2d(0,0);
+    private static Vector2d TARGET_POS = new Vector2d(0,75);
     private static Vector2d CURRENT_POS = new Vector2d(0,75);
 
     public static void initialize(){
@@ -39,6 +43,11 @@ public class HudGui {
     private static void render(DrawContext context, RenderTickCounter tickCounter){
 
         MinecraftClient client = MinecraftClient.getInstance();
+        TextRenderer fontRenderer = client.textRenderer;
+
+        Matrix4f identityMatrix = new Matrix4f().identity();
+
+        VertexConsumerProvider vertexConsumerProvider = client.getBufferBuilders().getEntityVertexConsumers();
 
         double currentTime = Util.getMeasuringTimeMs() / 1000.0;
 
@@ -56,8 +65,21 @@ public class HudGui {
         int posX2 = (int) (((double) posX /screenWidth+DIMENSIONS.x)*screenWidth);
         int posY2 = (int) (((double) posY /screenHeight+DIMENSIONS.y)*screenHeight);
 
-
-
         context.fill(posX/100,posY/100,posX2/100,posY2/100, 0xFFFF0000);
+
+        fontRenderer.draw(
+                "I hate fabric!!",
+                posX+5,
+                   posY+5,
+                    0xFF404040,
+                true,
+                identityMatrix,
+                vertexConsumerProvider,
+                TextRenderer.TextLayerType.NORMAL,
+                Integer.MAX_VALUE,
+                0
+
+        );
+
     }
 }
