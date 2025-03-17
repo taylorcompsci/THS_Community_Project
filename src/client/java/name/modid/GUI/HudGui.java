838e39a0fd537dace515165c81d4090867d34306
Jaiden Khosla <jaiden@khosla.io>
2025-03-17 06:35:47 -0500

    working on tweening

--- src/client/java/name/modid/GUI/HudGui.java
+++ src/client/java/name/modid/GUI/HudGui.java
@@ -11,6 +11,7 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.LayeredDrawer;
 import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.entity.decoration.DisplayEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Util;
@@ -19,6 +20,8 @@ import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Position;
 import org.joml.Matrix4f;
 import org.joml.Vector2d;
+
+import java.awt.*;
 
 
 public class HudGui {
@@ -32,15 +35,29 @@ public class HudGui {
     private static Vector2d DIMENSIONS = new Vector2d(100,25);
     //Positions shift from the top left corner of element
     private static Vector2d TARGET_POS = new Vector2d(0,75);
-    private static Vector2d CURRENT_POS = new Vector2d(0,75);
+    private static Vector2d CURRENT_POS = new Vector2d(0,100);
+
+    //in milliseconds
+    private static double TWEEN_DURATION = 10000;
+
+    private static double startTime = -1;
 
     public static void initialize(){
         THSCommunityProject.LOGGER.info("Initializing the GUI.");
-        HudLayerRegistrationCallback.EVENT.register(layeredDrawer -> layeredDrawer.attachLayerBefore(IdentifiedLayer.CHAT, LAYER, HudGui::render));
+        HudLayerRegistrationCallback.EVENT.register(layeredDrawer -> layeredDrawer.attachLayerAfter(IdentifiedLayer.CHAT, LAYER, HudGui::render));
     }
 
 
     private static void render(DrawContext context, RenderTickCounter tickCounter){
+
+        if(startTime == -1){
+            startTime = Util.getMeasuringTimeMs();
+        }
+
+        double currentTime = Util.getMeasuringTimeMs()-startTime;
+
+        double progress = MathHelper.clamp(currentTime/TWEEN_DURATION, 0.0, 1.0);
+
 
         MinecraftClient client = MinecraftClient.getInstance();
         TextRenderer fontRenderer = client.textRenderer;
@@ -49,35 +66,33 @@ public class HudGui {
 
         VertexConsumerProvider vertexConsumerProvider = client.getBufferBuilders().getEntityVertexConsumers();
 
-        double currentTime = Util.getMeasuringTimeMs() / 1000.0;
-
         int screenWidth = client.getWindow().getScaledWidth();
         int screenHeight = client.getWindow().getScaledHeight();
 
+        CURRENT_POS.x = MathHelper.lerp(progress, CURRENT_POS.x, TARGET_POS.x);
+        CURRENT_POS.y = MathHelper.lerp(progress, CURRENT_POS.y, TARGET_POS.y);
+
+        int posX = (int) (screenWidth*(CURRENT_POS.x/100));
+        int posY = (int) (screenHeight*(CURRENT_POS.y/100));
+
+        int posX2 = (int) (screenWidth*((CURRENT_POS.x+DIMENSIONS.x)/100));
+        int posY2 = (int) (screenHeight*((CURRENT_POS.y+DIMENSIONS.y)/100));
 
 
-        int posX = (int) (screenWidth * MathHelper.lerp(currentTime, CURRENT_POS.x, TARGET_POS.x));
-        int posY = (int) (screenWidth * MathHelper.lerp(currentTime, CURRENT_POS.x, TARGET_POS.x));
 
-        CURRENT_POS.x = posX;
-        CURRENT_POS.y = posY;
-
-        int posX2 = (int) (((double) posX /screenWidth+DIMENSIONS.x)*screenWidth);
-        int posY2 = (int) (((double) posY /screenHeight+DIMENSIONS.y)*screenHeight);
-
-        context.fill(posX/100,posY/100,posX2/100,posY2/100, 0xFFFF0000);
+        context.fill(posX,posY,posX2,posY2, 0xFFFF0000);
 
         fontRenderer.draw(
                 "I hate fabric!!",
-                posX+5,
-                   posY+5,
-                    0xFF404040,
-                true,
+                5,
+                    5,
+                Color.WHITE.getRGB(),
+                false,
                 identityMatrix,
                 vertexConsumerProvider,
                 TextRenderer.TextLayerType.NORMAL,
                 Integer.MAX_VALUE,
-                0
+                200
 
         );
 
